7144f4498e6e4fede6c9b98348638c6235d839ae
sam47-asfaw <sami.amare@gmail.com>
2020-11-20 13:24:59 +0300

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDom from 'react-dom'
+
+//CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+function BookList(){
+  return( 
+    <section className="booklist">
+    <Book />
+    </section>
+    
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//explicit arrow function
+//nested components
+const Book = () => {
+  const title='I Love You to the Moon and Back';
+  const author='Amelia Hepworth';
+  return(
+    <article className='book'>
+    <img src="https://images-na.ssl-images-amazon.
+com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt=""/>
+    <h1>{title} </h1>
+    <h4>{author.toUpperCase()} </h4>
+  </article> 
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<BookList />,document.getElementById('root'));
